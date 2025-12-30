b52f624b54db16f891758a9a4ae994cd91063c29
WaywardFlame <bstatomb2@gmail.com>
2025-12-30 14:30:33 -0600

    first page stylization

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,30 @@ import "./CSS/App.css";
 
 function App(){
   return (
-    <div>
-      <SocialContainer pageLink="#" pageName="ABOUT ME">
-        <SocialItem socialURL="https://waywardflame.itch.io/" imagePath="Social Logos/itch io icon.png" />
-        <SocialItem socialURL="https://www.youtube.com/@WaywardFlame" imagePath="Social Logos/yt_icon_red_digital.png" />
-        <SocialItem socialURL="https://bsky.app/profile/waywardflame.bsky.social" imagePath="Social Logos/bluesky logo.png" />
-        <SocialItem socialURL="https://www.linkedin.com/in/brian-statom-33a351250" imagePath="Social Logos/linkedin logo.png" />
-        <SocialItem socialURL="https://www.github.com/WaywardFlame" imagePath="Social Logos/github logo.png" />
-        <SocialItem socialURL="#" imagePath="Social Logos/Gmail-logo.png" />
-      </SocialContainer>
+    <div className="PageContainer">
+      <div className="SocialDivContainer">
+        <div className="ProfileDivContainer">
+          <img id="ProfileImage" src="Social Logos/lost candle profile pic.png" alt="Profile Image" />
+        </div>
+        <SocialContainer pageLink="#" pageName="ABOUT ME">
+          <SocialItem socialURL="https://waywardflame.itch.io/" imagePath="Social Logos/itch io icon.png" />
+          <SocialItem socialURL="https://www.youtube.com/@WaywardFlame" imagePath="Social Logos/yt_icon_red_digital.png" />
+          <SocialItem socialURL="https://bsky.app/profile/waywardflame.bsky.social" imagePath="Social Logos/bluesky logo.png" />
+          <SocialItem socialURL="https://www.linkedin.com/in/brian-statom-33a351250" imagePath="Social Logos/linkedin logo.png" />
+          <SocialItem socialURL="https://www.github.com/WaywardFlame" imagePath="Social Logos/github logo.png" />
+          <SocialItem socialURL="#" imagePath="Social Logos/Gmail-logo.png" />
+        </SocialContainer>
+      </div>
 
-      <ProjectPreviewList>
-        <ProjectPreviewItem previewTitle="Ultra Gangster: A Twin Stick Shooter Game" previewText="A simple game I made while trying to learn Godot and C#." imagePath="Project Pictures/Ultra Gangster 2D/UltraGangster2DPic.png" />
-        <ProjectPreviewItem previewTitle="The Steel Hearted" previewText="A simple turned based game I made as part of a game programming class." imagePath="Project Pictures/SteelHeartedPic.png" />
-        <ProjectPreviewItem previewTitle="Aria of the Steel Hearted" previewText="Builds upon the world I came up before working on The Steel Hearted." imagePath="Project Pictures/Aria Script Preview.png" />
-      </ProjectPreviewList>
-
-      <img id="ProfileImage" src="Social Logos/lost candle profile pic.png" alt="Profile Image" />
+      <div className="ProjectDivContainer">
+        <p className="HeadingText" id="Heading">WaywardFlame / Brian T. Statom</p>
+        <p className="HeadingText" id="Subtitle">Some projects I've worked on. You can view all of them <a href="#">{"-->here.<--"}</a></p>
+        <ProjectPreviewList>
+          <ProjectPreviewItem previewTitle="Ultra Gangster: A Twin Stick Shooter Game" previewText="A simple game I made while trying to learn Godot and C#." imagePath="Project Pictures/Ultra Gangster 2D/UltraGangster2DPic.png" />
+          <ProjectPreviewItem previewTitle="The Steel Hearted" previewText="A simple turned based game I made as part of a game programming class." imagePath="Project Pictures/SteelHeartedPic.png" />
+          <ProjectPreviewItem previewTitle="Aria of the Steel Hearted" previewText="Builds upon the world I came up before working on The Steel Hearted." imagePath="Project Pictures/Aria Script Preview.png" />
+        </ProjectPreviewList>
+      </div>
     </div>
   );
 }
